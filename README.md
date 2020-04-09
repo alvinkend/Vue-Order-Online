18cbcccebd55f0a0734d45b8957a4c2abedc74c2
Ricky Alvin <rickyalvin10@gmail.com>
2020-04-09 14:38:43 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Online-Order-FS
+
+## Installation
+```sh
+client
+yarn install
+yarn serve
+
+server
+yarn install
+yarn dev
+
+migrate & seed
+npx sequelize-cli db:migrate
+npx sequelize-cli db:seed:all
+
+Harus jalan berdua secara bersamaan, lalu buka localhost:8080
+```
---
+++ client/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <header class="jumbotron">
+      <h2>{{content}}</h2>
+    </header>
+  </div>
+</template>
+
+<script>
+import UserService from '../services/user.service';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      content: ''
+    };
+  },
+  mounted() {
+    UserService.getPublicContent().then(
+      response => {
+        this.content = response.data.message;
+      },
+      error => {
+        this.content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+      }
+    );
+  }
+};
+</script>
